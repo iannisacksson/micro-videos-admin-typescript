1b44b5cdc7d2ea27f4dfb29f289183195f368107
Iann Isacksson <iisackssonoliveira@gmail.com>
2024-07-24 22:36:57 -0300

    feat: creating custom error for when entity does not exist

--- src/shared/domain/value-objects/uuid.vo.ts
+++ src/shared/domain/value-objects/uuid.vo.ts
@@ -1,25 +1,30 @@
-import { ValueObject } from '../value-object';
-import { v4 as uuidV4, validate as uuidValidate } from "uuid";
+import { ValueObject } from "../value-object";
+import { v4 as uuidv4, validate as uuidValidate } from "uuid";
 
 export class Uuid extends ValueObject {
-    readonly id: string;
-    constructor(id?: string) {
-        super();
-        this.id = id || uuidV4();
-        this.validate()
-    }
+  readonly id: string;
 
-    private validate() {
-        const isValid = uuidValidate(this.id);
-        if (!isValid) {
-            throw new InvalidUuidError();
-        }
+  constructor(id?: string) {
+    super();
+    this.id = id || uuidv4();
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = uuidValidate(this.id);
+    if (!isValid) {
+      throw new InvalidUuidError();
     }
+  }
+
+  toString() {
+    return this.id;
+  }
 }
 
 export class InvalidUuidError extends Error {
-    constructor(message?: string) {
-        super(message || "ID must be a valide UUID");
-        this.name = "InvalidUuidError"
-    }
+  constructor(message?: string) {
+    super(message || "ID must be a valida UUID");
+    this.name = "InvalidUuidError";
+  }
 }
